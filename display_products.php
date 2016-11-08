279bc3c2b6227cac408130f84cb7ead884d3f16a
Alex <klimen1@umbc.edu>
2016-11-07 22:35:45 -0500

    table layout for product page

--- display_products.php
+++ display_products.php
@@ -34,22 +34,50 @@ if (!$items)
 }  
 
 // due to time constraints hard-coded for the demo/testing purpose
-$counter = 1;
-
+//$counter = 1;
+$i = 1;
+$displayAllProducts = "";
 if ($items->num_rows > 0) 
 {
     // print out items 3 in a row
     while($row = $items->fetch_assoc()) 
     {	
+     //   echo "<p style='display: inline;'>" . $row["item_name"];
+     //   echo "<img src=" . $row["image_link"] . " alt=" . $row["item_name"] . " height='200' width='200' " . ">" . "</p>";
 
-        echo "<p style='display: inline;'>" . $row["item_name"];
-        echo "<img src=" . $row["image_link"] . " alt=" . $row["item_name"] . " height='200' width='200' " . ">" . "</p>";
+	if($i%3 == 1) {   // If number is 1,4,7,etc start a new row
+	$displayAllProducts.= "<tr>"; 
+	}
+	$displayAllProducts.=
+	    "
+		<td>    
+		<table>  
+		<tr><td><center>" .$row['item_name'] . "</center></td></tr>" .
+		"<tr><td colspan ='2'><img src=" . "'" . $row["image_link"] . "'" . " width = '200' height = '200'" . "></td></tr>
+		</table> 
+		</td>  
+	     ";
+	if($i%3 == 0) {   // If number is 3,6,9,etc close the row
+	$displayAllProducts.= "</tr>";
+	}
+	$i++; // increase the counter to start again
+	
+	}  // ends the loop
 
-        if ($counter%3==0) { echo "<br>"; }
-        $counter++;
+	if($i%3 != 0){
+	$displayAllProducts.= "</tr>";	
+	}
+	
+
+     //   if ($counter%3==0) { echo "<br>"; }
+     //   $counter++;
+	echo "<table>";
+	print $displayAllProducts;
+	echo "</table>";
     }
-
-} 
+	$file = "/opt/lampp/temp/allproducts.html";
+	touch($file);
+	file_put_contents($file, $displayAllProducts);
 
 // close the connection
 $conn->close();
