e8cd75cd45a150d66cac778d9f1d49b243a38f02
Alex <klimen1@umbc.edu>
2016-12-07 12:05:57 -0500

    in upload item page, manufacturer was added

--- php/show_uploadItem.php
+++ php/show_uploadItem.php
@@ -1,52 +1,4 @@
-
 <?php
-//information for connecting to database
-$servername = "104.236.58.254";
-$username = "umbcxchange";
-$password = "umbcxchange";
-$DBname = "umbcxchange";
-$row = "";
-
-$firstname = "";
-$lastname = "";
-$ID = "";
-$user_since = "";
-$rating = "";
-$purchased = "";
-
-// Create connection to the database
-$conn = new mysqli($servername, $username, $password, $DBname);
-
-// Validate the connection
-if ($conn->connect_error)
-{
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// temporarily hard-coded
-$user_query = "SELECT * FROM  `Users` WHERE  `id` = 0";
-$user = $conn->query($user_query);
-
-if (!$user) {  exit('<p> Error: ' . mysql_error() . '</p>'); }
-
-if ($user->num_rows > 0)
-{
-    while($row = $user->fetch_assoc())
-    {
-
-		$firstname = $row["firstname"] ;
-		$lastname = $row["lastname"] ;
-		$ID =  $row["umbcid"];
-		$user_since = $row["register_date"];
-    $last_login = $row["last_login_date"];
-		$rating = $row["rating"];
-    $purchased = $row["products_bought"];
-    $sold = $row["products_sold"];
-    }
-
-}
-
-$conn->close(); // close the connection
 ?>
 
 <!-- Container for user details -->
@@ -75,6 +27,13 @@ $conn->close(); // close the connection
     <input id="itemName" name="item_name" type="text" placeholder="Item Name" class="form-control input-md" >
   </div>
 </div>
+<div class="form-group">
+  <label class="col-md-4 control-label" for="itemManufacturer">Manufacturer</label>
+  <div class="col-md-4">
+    <input id="itemManufacturer" name="item_manufacturer" type="text" placeholder="Item Manufacturer" class="form-control input-md" >
+  </div>
+</div>
+
 <!--
 <div class="form-group">
 <label class="col-md-4 control-label" for="selectCategory">Select Category</label>
